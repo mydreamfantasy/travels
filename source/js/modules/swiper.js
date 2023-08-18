78f5c43d08ed8c21e4326a5aa9bcf8a94f18f71a
Anastasiia <nastyara13@yandex.ru>
2023-08-18 21:39:59 +0300

    features and gallery

--- source/js/modules/swiper.js
+++ source/js/modules/swiper.js
@@ -93,3 +93,93 @@ export const getSwiperLearn = () => {
     swiper.init();
   }
 };
+
+export const getSwiperReviews = () => {
+  if (!swiperContainer) {
+    return;
+  } else {
+    const swiper = new Swiper('[data-swiper="reviews"]', {
+      direction: "horizontal",
+      watchOverflow: true,
+      loop: false,
+      autoHeight: true,
+      spaceBetween: 30,
+
+      navigation: {
+        prevEl: ".reviews__button--next",
+        nextEl: ".reviews__button--prev",
+      },
+
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+          initialSlide: 0,
+        },
+
+        768: {
+          slidesPerView: "auto",
+          centeredSlides: true,
+        },
+      },
+    });
+
+    swiper.init();
+  }
+};
+
+export const getSwiperFeatures = () => {
+  if (!swiperContainer) {
+    return;
+  } else {
+    const swiper = new Swiper('[data-swiper="features"]', {
+      breakpoints: {
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+          initialSlide: 0.5,
+          direction: "horizontal",
+          watchOverflow: true,
+          loop: true,
+          autoHeight: true,
+
+          navigation: {
+            prevEl: ".features__button--next",
+            nextEl: ".features__button--prev",
+          },
+        },
+      },
+    });
+
+    swiper.init();
+    const duplicates = document.querySelectorAll(".swiper-slide-duplicate");
+    duplicates.forEach((el) => el.removeAttribute("tabindex"));
+  }
+};
+
+export const getSwiperGallery = () => {
+  if (!swiperContainer) {
+    return;
+  } else {
+    const swiper = new Swiper('[data-swiper="gallery"]', {
+      slidesPerView: "auto",
+      spaceBetween: 5,
+      loop: true,
+      navigation: {
+        nextEl: ".gallery__button--prev",
+        prevEl: ".gallery__button--next",
+      },
+
+      breakpoints: {
+        320: {
+          spaceBetween: 3,
+        },
+
+        768: {
+          spaceBetween: 5,
+        },
+      },
+    });
+    swiper.init();
+  }
+};
